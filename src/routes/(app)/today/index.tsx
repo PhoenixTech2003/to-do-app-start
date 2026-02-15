594797b52af68d4b855abc1dd67be73f11009e4f
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2026-02-15 12:06:23 +0200

    Setup the scafolding for the today implementation

--- src/routes/(app)/today/index.tsx
+++ src/routes/(app)/today/index.tsx
@@ -1,9 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { BackButton } from '@/components/app/back-button'
 
 export const Route = createFileRoute('/(app)/today/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/today/"!</div>
+  return (
+    <header className="mb-6 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <BackButton />
+        <div>
+          <h2 className="text-2xl font-semibold">Twodo's for Today</h2>
+        </div>
+      </div>
+      {/* <CreateTodoDialog listId={listId as Id<'lists'>} /> */}
+    </header>
+  )
 }
