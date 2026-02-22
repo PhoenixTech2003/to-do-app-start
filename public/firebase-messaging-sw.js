bf8e97a87198566db712e594ea286b0b2d6a1f30
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2026-02-22 21:59:17 +0200

    Add more config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,3 +6,17 @@ import firebase from "firebase"
 firebase.initializeApp({
   messagingSenderId: '877820099842',
 })
+
+const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Message received:', payload)
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/favicon.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+})
