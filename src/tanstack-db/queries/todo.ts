2c7e553d8589e2237e7d3a7df87e19f7ec2935d4
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2026-02-22 08:02:16 +0200

    Remove the tanstack db

--- src/tanstack-db/queries/todo.ts
+++
@@ -1,6 +0,0 @@
-import { pendingTodosCollection } from '../collections/todo'
-import type { InitialQueryBuilder } from '@tanstack/db'
-import type { Id } from 'convex/_generated/dataModel'
-
-export const getPendingTodos = (q: InitialQueryBuilder, listId: Id<'lists'>) =>
-  q.from({ pendingTodos: pendingTodosCollection(listId) })
