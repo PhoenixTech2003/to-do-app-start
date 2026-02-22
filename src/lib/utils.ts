7ddc800c72d48e535195e811bfb174c741ee7bbe
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2026-02-22 07:17:53 +0200

    Add text truncation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from 'clsx'
+import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import type { ClassValue } from 'clsx'
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs))
+}
+
+export function truncateText(text: string | undefined, max = 50): string {
+  if (!text) return ''
+  return text.length > max ? `${text.slice(0, max)}...` : text
 }
