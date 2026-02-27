ce8e9759c0d1b0dc19a4150dd59a21a18b42b775
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2026-02-27 21:42:40 +0200

    Move the search the bar up top

--- src/components/app/search-box.tsx
+++ src/components/app/search-box.tsx
@@ -17,7 +17,7 @@ export function SearchInput() {
   }, [visible])
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex justify-center px-4 py-4">
+    <div className="fixed top-0 left-0 right-0 flex justify-center px-4 py-4">
       {visible && (
         <motion.div
           initial={{ opacity: 0, y: 10 }}
